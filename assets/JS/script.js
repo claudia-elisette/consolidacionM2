f87af7e910611bb39573e6410ad8339762db4742
Claudia Elisette <claudiae.urbinag@gmail.com>
2024-07-18 01:30:19 -0400

    section home video

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,18 +2,21 @@
 
 function mostrarHabilidades(){
     document.getElementById("habilidades").style.display = "block";
+    document.getElementById("home").style.display = "none";
     document.getElementById("proyectos").style.display = "none";
     document.getElementById("formulario").style.display = "none";
 }
 
 function mostrarProyectos(){
     document.getElementById("proyectos").style.display = "block";
+    document.getElementById("home").style.display = "none";
     document.getElementById("habilidades").style.display = "none";
     document.getElementById("formulario").style.display = "none";
 }
 
 function mostrarFormulario(){
     document.getElementById("formulario").style.display = "block";
+    document.getElementById("home").style.display = "none";
     document.getElementById("habilidades").style.display = "none";
     document.getElementById("proyectos").style.display = "none";
 }
